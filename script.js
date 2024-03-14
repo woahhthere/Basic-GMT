4653ce9024a2c6d1d4ccbba8fc4d9ae1826a8cb3
ruzzelshane <ruzzelshanecalinawan@gmail.com>
2024-03-14 15:01:47 +0800

    header 100%

---
+++ script.js
@@ -0,0 +1,8 @@
+const burgir = document.querySelector(".burgir_menu")
+burgir.addEventListener("click", () =>{
+    burgir.classList.toggle("open");
+})
+const nav = document.querySelector(".header_nav")
+burgir.addEventListener("click", () =>{
+    nav.classList.toggle("open");
+})
